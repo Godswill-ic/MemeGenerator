d59a7ef8ce7ec771578590e429da398eb85b2360
Godswill-ic <gchukwueke01@gmail.com>
2024-10-08 12:57:10 +0100

    New project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Components/Header';
+import Generator from './Components/Generator';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Generator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function Header() {
+  return (
+    <div className='head'>
+      <div className='head--inner'>
+        <img src='../troll--face.png' alt='' />
+        <h3>MemeGenerator</h3>
+      </div>
+
+      <p>React Course - Project</p>
+    </div>
+  );
+}
+
+export default Header;
